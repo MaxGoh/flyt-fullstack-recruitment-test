d872b24f04bcbcff8c7077d3b3804213d68fd9a2
Max Goh <max@parally.sg>
2018-04-07 00:29:34 +0800

    First two row completed

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -13,20 +13,25 @@ class App extends React.Component<{}, {}> {
         <BottomBorderDiv borderHeight='4px'>
 
           <div className='header'>
-
+            {/*Item 1*/}
             <div className='header__item' />
-
+            {/*Item 2*/}
             <div className='header__item logo'>
               <img id='logo' src={Logo} alt='logo' />
             </div>
-
+            {/*Item 3*/}
             <div className='header__item profile'>
-                <img id='profile' src={Profile} alt='profile' />
+                <img className='profile__image' src={Profile} alt='profile' />
                 <a className='profile__login-text' href='#'><b>LOGIN</b></a>
             </div>
-
           </div>
 
+        </BottomBorderDiv>
+
+        <BottomBorderDiv borderHeight='3px'>
+          <div style={{ textAlign: 'center' }}>
+            <h1 style={{ margin: 0 }}>ORDER & COLLECT</h1>
+          </div>
         </BottomBorderDiv>
       </div>
     )
