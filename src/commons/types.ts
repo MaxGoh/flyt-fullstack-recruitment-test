fc311f7fe2b679a6fb94efc3c2cd0c5073302241
Max Goh <max@parally.sg>
2018-04-08 09:18:50 +0800

    Added Response type interface

--- src/commons/types.ts
+++ src/commons/types.ts
@@ -3,117 +3,59 @@ export interface ILocationMenu {
     id: Number
     name: String
   }
-
+  menus: IMenu[]
+  revision: Number
+  availability: {}
+  menusAvailable: Number[]
 }
 
 export interface IMenu {
-
+  id: Number
+  name: String
+  tags: String
+  categories: IMenuCategories[]
 }
 
-// {
-//   "orderType": {
-//     "id": 1,
-//     "name": "Order Ahead"
-//   },
-//   "menus": [
-//     {
-//       "id": 0,
-//       "name": "string",
-//       "categories": [
-//         {
-//           "id": 0,
-//           "name": "Burgers",
-//           "items": [
-//             {
-//               "id": 0,
-//               "name": "Home Made Burger",
-//               "description": "Our super home made burger",
-//               "customisationType": "simple",
-//               "portions": [
-//                 {
-//                   "id": 0,
-//                   "name": "Super Size Burger",
-//                   "ingredient": {
-//                     "id": 0,
-//                     "name": "Burger Patty",
-//                     "addPrice": 100,
-//                     "stockLevel": 1,
-//                     "slots": 1,
-//                     "modifiers": [
-//                       {
-//                         "id": 0,
-//                         "name": "Cook Type",
-//                         "requirementType": "required",
-//                         "inclusionType": "upgrade",
-//                         "visible": true,
-//                         "pickOptions": [
-//                           {
-//                             "id": 0,
-//                             "name": "Very rare",
-//                             "addPrice": 100,
-//                             "stockLevel": 1,
-//                             "slots": 0
-//                           }
-//                         ]
-//                       }
-//                     ]
-//                   }
-//                 }
-//               ]
-//             }
-//           ],
-//           "tags": "string"
-//         }
-//       ],
-//       "revision": 0,
-//       "upsells": [
-//         "string"
-//       ],
-//       "availability": {
-//         "regular": {
-//           "monday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "tuesday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "wednesday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "thursday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "friday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "saturday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           },
-//           "sunday": {
-//             "open": "07:00",
-//             "close": "23:59"
-//           }
-//         },
-//         "special": [
-//           {
-//             "date": "2016-12-24",
-//             "name": "Christmas Eve",
-//             "hours": {
-//               "open": "07:00",
-//               "close": "23:59"
-//             }
-//           }
-//         ]
-//       }
-//     }
-//   ],
-//   "menusAvailable": [
-//     0
-//   ]
-// }
+export interface IMenuCategories {
+  id: Number
+  name: String
+  items: IMenuCategoriesItem[]
+}
+
+export interface IMenuCategoriesItem {
+  id: Number
+  name: String
+  description: String
+  customisationType: String
+  portions: IMenuCategoriesItemPortion[]
+}
+
+export interface IMenuCategoriesItemPortion {
+  id: Number
+  name: String
+  ingredient: {
+    id: Number
+    name: String
+    addPrice: String
+    stockLevel: String
+    slots: String
+    modifiers: IMenuCategoriesItemPortionModifiers[]
+  }
+}
+
+export interface IMenuCategoriesItemPortionModifiers {
+  id: Number
+  name: String
+  requirementType: String
+  inclusionType: String
+  visible: Boolean
+  pickOptions: IMenuCategoriesItemPortionModifiersPickOptions[]
+}
+
+export interface IMenuCategoriesItemPortionModifiersPickOptions {
+  id: Number
+  name: String
+  addPrice: Number
+  stockLevel: String
+  slots: String
+}
