cabafd687f82656d0e817d294dbba60928d7f787
Max Goh <max@parally.sg>
2018-04-09 15:36:43 +0800

    Added Menu Descriptions

--- src/components/App/SelectFoodContainer/MenuList/index.tsx
+++ src/components/App/SelectFoodContainer/MenuList/index.tsx
@@ -26,27 +26,40 @@ class MenuList extends React.Component<IProps, {}> {
               return (
                 <div key={index}>
                   <BottomBorderDiv borderHeight='2px'>
-                    <div  className='item-cell' style={{ display: 'flex', justifyContent: 'space-between' }}>
-                      <div style={{ display: 'flex', flexDirection: 'row' }}>
-                        <h3 className='item-name'>
-                          {item.name}
-                        </h3>
-                        <div style={{ marginLeft: '5px' }}>
-                          {
-                            (item.portions !== undefined)
-                              ? item.portions.map((portion: commonTypes.IMenuCategoriesItemPortion, key: number) => {
-                                return (
-                                  <div key={key}>
-                                    <span style={{ fontSize: '14px' }}>£{portion.ingredient.addPrice}</span>
-                                  </div>
-                                )
-                                })
-                              : null
-                          }
+                    <div className='item-cell' style={{ display: 'flex', flexDirection: 'column' }}>
+                      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', flexDirection: 'row' }}>
+                          <h3 className='item-name'>
+                            {item.name}
+                          </h3>
+                          <div style={{ marginLeft: '5px' }}>
+                            {
+                              (item.portions !== undefined)
+                                ? item.portions.map((portion: commonTypes.IMenuCategoriesItemPortion, key: number) => {
+                                  return (
+                                    <div key={key}>
+                                      <span
+                                        style={{
+                                          fontSize: '14px', color: '#ef4135'
+                                        }}
+                                      >
+                                        £{portion.ingredient.addPrice}
+                                      </span>
+                                    </div>
+                                  )
+                                  })
+                                : null
+                            }
+                          </div>
+                        </div>
+                        <div>
+                          <AddItemButton item={item} />
                         </div>
                       </div>
                       <div>
-                        <AddItemButton item={item} />
+                        <p style={{ margin: 0 }}>
+                          {item.description}
+                        </p>
                       </div>
                     </div>
                   </BottomBorderDiv>
